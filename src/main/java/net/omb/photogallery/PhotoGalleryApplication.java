666f582a455798ed8f02fcc3f08c4cdaf34bda74
Volodymyr Bodnar <volodymyr.bodnar@itexpert.pl>
2017-07-31 09:50:54 +0200

    initial commit

---
+++ src/main/java/net/omb/photogallery/PhotoGalleryApplication.java
@@ -0,0 +1,12 @@
+package net.omb.photogallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoGalleryApplication.class, args);
+	}
+}
